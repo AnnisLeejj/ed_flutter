5963f1f005d4b96fec2d463d178fe6d6d1a2e02c
DESKTOP-9B4UMLR\Annis <l536510961@126.com>
2019-08-08 15:17:53 +0800

    首页框架搭建完成

--- lib/page/MainPage.dart
+++ lib/page/MainPage.dart
@@ -1,5 +1,10 @@
+import 'package:ed_flutter/page/home/MinePage.dart';
+import 'package:ed_flutter/utils/ToastUtil.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'home/HomePage.dart';
+import 'home/MessagePage.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -7,15 +12,77 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _currentPageIndex = 0;
+  var _pageController = new PageController(initialPage: 0);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Main"),
-      ),
-      body: Center(
-        child: Text("主页"),
+    return SafeArea(
+      child: new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text("我是AppBar"),
+//        centerTitle: true,
+//      ),
+        body: new PageView.builder(
+          onPageChanged: _pageChange,
+          controller: _pageController,
+          itemBuilder: (BuildContext context, int index) {
+            return getPage(index);
+          },
+          itemCount: 3,
+        ),
+        bottomNavigationBar: new BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: new Icon(Icons.home), title: new Text("任务")),
+            BottomNavigationBarItem(
+                icon: new Icon(Icons.message), title: new Text("消息")),
+            BottomNavigationBarItem(
+                icon: new Icon(Icons.portrait), title: new Text("我的")),
+          ],
+          currentIndex: _currentPageIndex,
+          onTap: onTap,
+        ),
       ),
     );
+  }
+
+  // bottomnaviagtionbar 和 pageview 的联动
+  void onTap(int index) {
+    _pageController.jumpToPage(index);
+    // 过pageview的pagecontroller的animateToPage方法可以跳转
+//    _pageController.animateToPage(index,
+//        duration: const Duration(milliseconds: 300), curve: Curves.ease);
+  }
+
+  void _pageChange(int index) {
+    setState(() {
+      if (_currentPageIndex != index) {
+        _currentPageIndex = index;
+      }
+    });
+  }
+
+  Widget first, second, third;
+
+  Widget getPage(int index) {
+    switch (index) {
+      case 0:
+        if (first == null) {
+          first = HomePage();
+        }
+        break;
+      case 1:
+        if (second == null) {
+          second = MessagePage();
+        }
+        break;
+      case 2:
+        if (third == null) {
+          third = MinePage();
+        }
+        break;
+    }
+    return index == 0 ? first : (index == 1 ? second : third);
   }
 }
---
+++ lib/page/home/MessagePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class MessagePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("我是消息页");
+  }
+}
